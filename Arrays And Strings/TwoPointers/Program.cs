ac03d100ce314dfd87518ca05341ba000c237858
Hudson Bean <hudsonbeandev@gmail.com>
2023-10-23 12:16:06 -0500

    Finished the TwoPointers in both Py and Cs

---
+++ Arrays And Strings/TwoPointers/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args) {
+        bool checkIfPalindrome(string word) {
+            int left = 0;
+            int right = word.Length - 1;
+            while (left < right) {
+                if (word[left] != word[right]) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+        Console.WriteLine(Convert.ToString(checkIfPalindrome("racecar")));
+    }
+}
